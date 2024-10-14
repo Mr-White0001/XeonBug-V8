54057c016f7d43be5dd1726a43f5a49f01d50d41
Mr-White0001 <iyobosawealth32@gmail.com>
2024-10-14 13:25:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "09058240320"
+global.ownername = "Mr White"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "Nigeria, Edo State, Benin City"
 
-global.ownernumber = '923444844060'  //creator number
+global.ownernumber = '09058240320'  //creator number
 global.ownername = 'Qasim Ali' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
